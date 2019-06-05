d75e7dd488781ed339d9fcaa2c95c9e7c903b628
alexey <lesha.kovalenko.1999@mail.ru>
2019-06-05 21:41:47 +0300

    last1

--- main.cpp
+++ main.cpp
@@ -1,63 +1,46 @@
+// Лабораторная работа 2 по дисциплине МРЗвИС
+// Выполнена студентом группы 721701
+// БГУИР Коваленко Алексеем Васильевичем
+// Реализация модели решения задачи на ОКМД архитектуре
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 
 using namespace std;
 
-int a_to_b(int a, int b);
-int b_to_a(int a, int b);
-int a_and_b(int a, int b);
-int f(int a, int b, int e);
-int d(int a, int b);
+double a_to_b(double a, double b);
+double a_and_b(double a, double b);
+double f(double a, double b, double e);
+double d(double a, double b);
+double ** createArray(int n, int m);
+void random(double **a, int n, int m);
+void view(double **a, int n, int m);
+
 
 int main() {
     srand(time(nullptr));
     int p_size = 0, m_size = 0, q_size = 0;
-    int **a, **b, *e, **g, **c;
+    double **a, **b, *e, **g, **c;
 
     cout << "Input p, m, q";
     cout << endl;
     cin >> p_size >> m_size >> q_size;
-    a = new int *[p_size];
-    b = new int *[m_size];
-    e = new int [m_size];
-    g = new int *[p_size];
-    c = new int *[p_size];
-    for (int i = 0; i < p_size; i++) {
-        a[i] = new int[m_size];
-        g[i] = new int[q_size];
-        c[i] = new int[q_size];
-    }
+    a = createArray(p_size, m_size);
+    b = createArray(m_size, q_size);
+    g = createArray(p_size, q_size);
+    c = createArray(p_size, q_size);
+    e = new double [m_size];
+    random(a, p_size, m_size);
+    random(b, m_size, q_size);
+    random(g, p_size, q_size);
     for (int i = 0; i < m_size; i++) {
-        b[i] = new int[q_size];
-    }
-    for (int i = 0; i < p_size; i++) {
-       for (int j = 0; j < m_size; j++) {
-           a[i][j] = rand() % 3 - 1;
-       }
-    }
-    for (int i = 0; i < m_size; i++) {
-        for (int j = 0; j < q_size; j++) {
-            b[i][j] = rand() % 3 - 1;
-        }
-    }
-    for (int i = 0; i < m_size; i++) {
-        e[i] = rand() % 3 - 1;
-    }
-    for (int i = 0; i < p_size; i++) {
-        for (int j = 0; j < q_size; j++) {
-            g[i][j] = rand() % 3 - 1;
-        }
-    }
-    for (int i = 0; i < p_size; i++) {
-        for (int j = 0; j < q_size; j++) {
-            c[i][j] = rand() % 3 - 1;
-        }
+        e[i] = (double)rand()/(double) RAND_MAX*2 - 1;
     }
 
     for (int i = 0; i < p_size; i++) {
         for (int j = 0; j < q_size; j++) {
-            int f1 = 1, d1 = 1;
+            double f1 = 1, d1 = 1;
             for(int k = 0; k  < m_size; k++){
                 f1 *= f(a[i][k], b[k][j], e[k]);
                 d1 *= 1 - d(a[i][k], b[k][j]);
@@ -67,57 +50,65 @@ int main() {
         }
     }
     cout << endl;
+    view(a, p_size, m_size);
+    view(b, m_size, q_size);
+    view(g, p_size, q_size);
+    for (int i = 0; i < m_size; i++) {
+        cout << e[i] << " ";
+    }
+    cout << endl;
+    cout << endl;
+    view(c, p_size, q_size);
     for (int i = 0; i < p_size; i++) {
-        for (int j = 0; j < m_size; j++) {
+        delete[] a[i];
+        delete[] g[i];
+        delete[] c[i];
+    }
+    for (int i = 0; i < m_size; i++) {
+        delete[] b[i];
+    }
+    delete[] e;
+    return 0;
+}
+
+void view(double **a, int n, int m){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             cout << a[i][j] << " ";
         }
         cout << endl;
     }
     cout << endl;
-    for (int i = 0; i < m_size; i++) {
-        for (int j = 0; j < q_size; j++) {
-            cout << b[i][j] << " ";
+}
+
+void random(double **a, int n, int m){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            a[i][j] = (double)rand()/(double) RAND_MAX*2 - 1;
         }
-        cout << endl;
     }
-    cout << endl;
-    for (int i = 0; i < m_size; i++) {
-            cout << e[i] << " ";
+}
+
+double ** createArray(int n, int m){
+    double **a = new double*[n];
+    for(int i = 0; i < n; i++){
+        a[i] = new double[m];
     }
-    cout << endl;
-    cout << endl;
-    for (int i = 0; i < p_size; i++) {
-        for (int j = 0; j < q_size; j++) {
-            cout << g[i][j] << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
-    for (int i = 0; i < p_size; i++) {
-        for (int j = 0; j < q_size; j++) {
-            cout << c[i][j] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
+    return a;
 }
 
-int a_to_b(int a, int b){
-    return 1-a ? b : (1-a > b);
+double a_to_b(double a, double b){
+    return (a + b - 1 > 1) ? 1 : a + b - 1;
 }
 
-int b_to_a(int a, int b){
-    return a ? 1-b : (a > 1-b);
+double a_and_b(double a, double b){
+    return (a + b - 1 > 0) ? a + b - 1  : 0;
 }
 
-int a_and_b(int a, int b){
-    return a ? b : (a < b);
+double f(double a, double b, double e){
+    return a_to_b(a, b)*(2*e - 1)*e + a_to_b(a, b)*(1 + 4*e*a_to_b(a, b))*(1 - e);
 }
 
-int f(int a, int b, int e){
-    return a_to_b(a, b)*(2*e - 1)*e + b_to_a(a, b)*(1 + 4*e*a_to_b(a, b))*(1 - e);
-}
-
-int d(int a, int b){
+double d(double a, double b){
     return a_and_b(a, b);
 }
